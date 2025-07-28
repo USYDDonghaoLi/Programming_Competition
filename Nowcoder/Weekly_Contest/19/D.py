d4df0a94d0c2e2d83258c06791d4a517021734de
Ldh <ldh1315109@gmail.com>
2025-07-28 23:08:17 +1000

    Upload Nowcoder Weekly Contest 19

---
+++ Nowcoder/Weekly_Contest/19/D.py
@@ -0,0 +1,176 @@
+'''
+Hala Madrid!
+https://www.zhihu.com/people/li-dong-hao-78-74
+'''
+
+import sys
+import os
+from io import BytesIO, IOBase
+BUFSIZE = 8192
+class FastIO(IOBase):
+    newlines = 0
+    def __init__(self, file):
+        self._fd = file.fileno()
+        self.buffer = BytesIO()
+        self.writable = "x" in file.mode or "r" not in file.mode
+        self.write = self.buffer.write if self.writable else None
+    def read(self):
+        while True:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            if not b:
+                break
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines = 0
+        return self.buffer.read()
+    def readline(self):
+        while self.newlines == 0:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            self.newlines = b.count(b"\n") + (not b)
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines -= 1
+        return self.buffer.readline()
+    def flush(self):
+        if self.writable:
+            os.write(self._fd, self.buffer.getvalue())
+            self.buffer.truncate(0), self.buffer.seek(0)
+class IOWrapper(IOBase):
+    def __init__(self, file):
+        self.buffer = FastIO(file)
+        self.flush = self.buffer.flush
+        self.writable = self.buffer.writable
+        self.write = lambda s: self.buffer.write(s.encode("ascii"))
+        self.read = lambda: self.buffer.read().decode("ascii")
+        self.readline = lambda: self.buffer.readline().decode("ascii")
+sys.stdin, sys.stdout = IOWrapper(sys.stdin), IOWrapper(sys.stdout)
+input = lambda: sys.stdin.readline().rstrip("\r\n")
+
+def I():
+    return input()
+def II():
+    return int(input())
+def MI():
+    return map(int, input().split())
+def LI():
+    return list(input().split())
+def LII():
+    return list(map(int, input().split()))
+def GMI():
+    return map(lambda x: int(x) - 1, input().split())
+
+#------------------------------FastIO---------------------------------
+
+from bisect import *
+from heapq import *
+from collections import *
+from functools import *
+from itertools import *
+from time import *
+from random import *
+from math import log, gcd, sqrt, ceil
+
+# '''
+# 手写栈防止recursion limit
+# 注意要用yield 不要用return
+# 函数结尾要写yield None
+# '''
+# from types import GeneratorType
+# def bootstrap(f, stack=[]):
+#     def wrappedfunc(*args, **kwargs):
+#         if stack:
+#             return f(*args, **kwargs)
+#         else:
+#             to = f(*args, **kwargs)
+#             while True:
+#                 if type(to) is GeneratorType:
+#                     stack.append(to)
+#                     to = next(to)
+#                 else:
+#                     stack.pop()
+#                     if not stack:
+#                         break
+#                     to = stack[-1].send(to)
+#             return to
+#     return wrappedfunc
+
+# RANDOM = getrandbits(32)
+ 
+# class Wrapper(int):
+#     def __init__(self, x):
+#         int.__init__(x)
+
+#     def __hash__(self):
+#         return super(Wrapper, self).__hash__() ^ RANDOM
+
+# def TIME(f):
+
+#     def wrap(*args, **kwargs):
+#         s = perf_counter()
+#         ret = f(*args, **kwargs)
+#         e = perf_counter()
+
+#         print(e - s, 'sec')
+#         return ret
+    
+#     return wrap
+d = ((1, 0), (0, 1), (-1, 0), (0, -1), (1, 1), (1, -1), (-1, 1), (-1, -1))
+# @TIME
+def solve(testcase):
+    grid = [list(I()) for _ in range(4)]
+
+    tot = 0
+    grid2 = [[0 for _ in range(4)] for _ in range(4)]
+
+    def check(g):
+        for r in range(4):
+            for c in range(4):
+                if grid[r][c] == '.':
+                    pass
+                else:
+                    # print('rc', r, c, grid[r][c], g[r][c])
+                    if g[r][c] == 1:
+                        return False
+                    else:
+                        # print('rc2', r, c)
+                        cnt = int(grid[r][c])
+                        for dr, dc in d:
+                            nr, nc = r + dr, c + dc
+                            if 0 <= nr < 4 and 0 <= nc < 4 and g[nr][nc] == 1:
+                                if grid[nr][nc] != '.':
+                                    return False
+                                cnt -= 1
+                        if cnt:
+                            return False
+        return True
+    
+    # print(check([[0, 0, 0, 0], [0, 1, 0, 1], [1, 1, 1, 1], [1, 1, 1, 1]]))
+
+    for state in range(1 << 16):
+        tmp = [[0 for _ in range(4)] for _ in range(4)]
+        for i in range(16):
+            r, c = divmod(i, 4)
+            tmp[r][c] = state >> i & 1
+        
+        if check(tmp):
+            tot += 1
+            for r in range(4):
+                for c in range(4):
+                    grid2[r][c] += tmp[r][c]
+    
+    # print('tot', tot)
+    # print('grid2', grid2)
+
+    for i in range(4):
+        for j in range(4):
+            if grid[i][j] == '.':
+                if tot == grid2[i][j]:
+                    grid[i][j] = 'X'
+                if grid2[i][j] == 0:
+                    grid[i][j] = 'O'
+    
+    for i in range(4):
+        print(''.join(grid[i]))
+
+for testcase in range(1):
+    solve(testcase)
