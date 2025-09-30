2cce272947a9625bc614b98512aaf00fbf33c908
Ldh <ldh1315109@gmail.com>
2025-10-01 01:04:57 +1000

    Upload Nowcoder Contest L

--- Nowcoder/Other_Contest/2025年香港城市大学（东莞）新生程序设计个人排位赛正式赛（同步赛）/L.py
+++ Nowcoder/Other_Contest/2025年香港城市大学（东莞）新生程序设计个人排位赛正式赛（同步赛）/L.py
@@ -116,161 +116,33 @@ inf = float('inf')
 fmin = lambda x, y: x if x < y else y
 fmax = lambda x, y: x if x > y else y
 
-class lazy_segtree():
-    def update(self,k):self.d[k]=self.op(self.d[2*k],self.d[2*k+1])
-    def all_apply(self,k,f):
-        self.d[k]=self.mapping(f,self.d[k])
-        if (k<self.size):self.lz[k]=self.composition(f,self.lz[k])
-    def push(self,k):
-        self.all_apply(2*k,self.lz[k])
-        self.all_apply(2*k+1,self.lz[k])
-        self.lz[k]=self.identity
-    def __init__(self,V,OP,E,MAPPING,COMPOSITION,ID):
-        self.n=len(V)
-        self.log=(self.n-1).bit_length()
-        self.size=1<<self.log
-        self.d=[E for i in range(2*self.size)]
-        self.lz=[ID for i in range(self.size)]
-        self.e=E
-        self.op=OP
-        self.mapping=MAPPING
-        self.composition=COMPOSITION
-        self.identity=ID
-        for i in range(self.n):self.d[self.size+i]=V[i]
-        for i in range(self.size-1,0,-1):self.update(i)
-    def set(self,p,x):
-        assert 0<=p and p<self.n
-        p+=self.size
-        for i in range(self.log,0,-1):self.push(p>>i)
-        self.d[p]=x
-        for i in range(1,self.log+1):self.update(p>>i)
-    def get(self,p):
-        assert 0<=p and p<self.n
-        p+=self.size
-        for i in range(self.log,0,-1):self.push(p>>i)
-        return self.d[p]
-    def prod(self,l,r):
-        assert 0<=l and l<=r and r<=self.n
-        if l==r:return self.e
-        l+=self.size
-        r+=self.size
-        for i in range(self.log,0,-1):
-            if (((l>>i)<<i)!=l):self.push(l>>i)
-            if (((r>>i)<<i)!=r):self.push(r>>i)
-        sml,smr=self.e,self.e
-        while(l<r):
-            if l&1:
-                sml=self.op(sml,self.d[l])
-                l+=1
-            if r&1:
-                r-=1
-                smr=self.op(self.d[r],smr)
-            l>>=1
-            r>>=1
-        return self.op(sml,smr)
-    def all_prod(self):return self.d[1]
-    def apply_point(self,p,f):
-        assert 0<=p and p<self.n
-        p+=self.size
-        for i in range(self.log,0,-1):self.push(p>>i)
-        self.d[p]=self.mapping(f,self.d[p])
-        for i in range(1,self.log+1):self.update(p>>i)
-    def apply(self,l,r,f):
-        assert 0<=l and l<=r and r<=self.n
-        if l==r:return
-        l+=self.size
-        r+=self.size
-        for i in range(self.log,0,-1):
-            if (((l>>i)<<i)!=l):self.push(l>>i)
-            if (((r>>i)<<i)!=r):self.push((r-1)>>i)
-        l2,r2=l,r
-        while(l<r):
-            if (l&1):
-                self.all_apply(l,f)
-                l+=1
-            if (r&1):
-                r-=1
-                self.all_apply(r,f)
-            l>>=1
-            r>>=1
-        l,r=l2,r2
-        for i in range(1,self.log+1):
-            if (((l>>i)<<i)!=l):self.update(l>>i)
-            if (((r>>i)<<i)!=r):self.update((r-1)>>i)
-    def max_right(self,l,g):
-        assert 0<=l and l<=self.n
-        assert g(self.e)
-        if l==self.n:return self.n
-        l+=self.size
-        for i in range(self.log,0,-1):self.push(l>>i)
-        sm=self.e
-        while(1):
-            while(l%2==0):l>>=1
-            if not(g(self.op(sm,self.d[l]))):
-                while(l<self.size):
-                    self.push(l)
-                    l=(2*l)
-                    if (g(self.op(sm,self.d[l]))):
-                        sm=self.op(sm,self.d[l])
-                        l+=1
-                return l-self.size
-            sm=self.op(sm,self.d[l])
-            l+=1
-            if (l&-l)==l:break
-        return self.n
-    def min_left(self,r,g):
-        assert (0<=r and r<=self.n)
-        assert g(self.e)
-        if r==0:return 0
-        r+=self.size
-        for i in range(self.log,0,-1):self.push((r-1)>>i)
-        sm=self.e
-        while(1):
-            r-=1
-            while(r>1 and (r%2)):r>>=1
-            if not(g(self.op(self.d[r],sm))):
-                while(r<self.size):
-                    self.push(r)
-                    r=(2*r+1)
-                    if g(self.op(self.d[r],sm)):
-                        sm=self.op(self.d[r],sm)
-                        r-=1
-                return r+1-self.size
-            sm=self.op(self.d[r],sm)
-            if (r&-r)==r:break
-        return 0
-
 # @TIME
 def solve(testcase):
     n, q = MI()
     A = LII()
 
-    B = [inf]
-    for i in range(1, n):
-        B.append(A[i] - A[i - 1])
-    
-    C = []
-    for i in range(n - 1):
-        C.append(A[i] - A[i + 1])
-    C.append(inf)
-
-    D = [int(B[i] * C[i]) > 0 for i in range(n)]
-
-    sg = lazy_segtree(
-        (
-            (a, b, c, d) for a, b, c, d in zip(A, B, C, D)
-        ),
-        lambda x, y: (
-            x[0] + y[0],
-            x[1] + y[1],
-            x[2] + y[2],
-            x[3] + y[3]
-        ),
-        (0, 0, 0, 0),
-        lambda y, x: (
+    for _ in range(q):
+        ops = LII()
+        if ops[0] == 1:
+            l, r, v = ops[1] - 1, ops[2] - 1, ops[3]
+            a = v
+            for i in range(l, r + 1):
+                A[i] += a
+                a += v
+        elif ops[0] == 2:
+            l, r, v = ops[1] - 1, ops[2] - 1, ops[3]
+            a = v
+            for i in range(l, r + 1):
+                A[i] = a
+                a += v
+        else:
+            l, r = ops[1] - 1, ops[2] - 1
+            res = 0
+            for i in range(l, r + 1):
+                if i > 0 and i < n - 1 and (A[i] - A[i - 1]) * (A[i] - A[i + 1]) > 0:
+                    res += 1
             
-        )
-    )
+            print(res)
 
 for testcase in range(1):
     solve(testcase)
