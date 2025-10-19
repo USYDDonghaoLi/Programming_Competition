cba6755c69614f31357c13527058c6ea37e78d65
Ldh <ldh1315109@gmail.com>
2025-10-19 21:47:30 +1100

    Upload Codes for Nowcoder Contest

---
+++ Nowcoder/Other_Contest/牛客2025秋季算法编程训练联赛1-提升组/C.py
@@ -0,0 +1,198 @@
+'''
+Hala Madrid!
+https://www.zhihu.com/people/li-dong-hao-78-74
+'''
+
+import sys
+import os
+from io import BytesIO, IOBase
+BUFSIZE = 8192
+class FastIO(IOBase):
+    newlines = 0
+    def __init__(self, file):
+        self._fd = file.fileno()
+        self.buffer = BytesIO()
+        self.writable = "x" in file.mode or "r" not in file.mode
+        self.write = self.buffer.write if self.writable else None
+    def read(self):
+        while True:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            if not b:
+                break
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines = 0
+        return self.buffer.read()
+    def readline(self):
+        while self.newlines == 0:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            self.newlines = b.count(b"\n") + (not b)
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines -= 1
+        return self.buffer.readline()
+    def flush(self):
+        if self.writable:
+            os.write(self._fd, self.buffer.getvalue())
+            self.buffer.truncate(0), self.buffer.seek(0)
+class IOWrapper(IOBase):
+    def __init__(self, file):
+        self.buffer = FastIO(file)
+        self.flush = self.buffer.flush
+        self.writable = self.buffer.writable
+        self.write = lambda s: self.buffer.write(s.encode("ascii"))
+        self.read = lambda: self.buffer.read().decode("ascii")
+        self.readline = lambda: self.buffer.readline().decode("ascii")
+sys.stdin, sys.stdout = IOWrapper(sys.stdin), IOWrapper(sys.stdout)
+input = lambda: sys.stdin.readline().rstrip("\r\n")
+
+def I():
+    return input()
+def II():
+    return int(input())
+def MI():
+    return map(int, input().split())
+def LI():
+    return list(input().split())
+def LII():
+    return list(map(int, input().split()))
+def GMI():
+    return map(lambda x: int(x) - 1, input().split())
+
+#------------------------------FastIO---------------------------------
+
+from bisect import *
+from heapq import *
+from collections import *
+from functools import *
+from itertools import *
+from time import *
+from random import *
+from math import log, gcd, sqrt, ceil
+
+# from types import GeneratorType
+# def bootstrap(f, stack=[]):
+#     def wrappedfunc(*args, **kwargs):
+#         if stack:
+#             return f(*args, **kwargs)
+#         else:
+#             to = f(*args, **kwargs)
+#             while True:
+#                 if type(to) is GeneratorType:
+#                     stack.append(to)
+#                     to = next(to)
+#                 else:
+#                     stack.pop()
+#                     if not stack:
+#                         break
+#                     to = stack[-1].send(to)
+#             return to
+#     return wrappedfunc
+
+# seed(19981220)
+# RANDOM = getrandbits(64)
+ 
+# class Wrapper(int):
+#     def __init__(self, x):
+#         int.__init__(x)
+
+#     def __hash__(self):
+#         return super(Wrapper, self).__hash__() ^ RANDOM
+
+# def TIME(f):
+
+#     def wrap(*args, **kwargs):
+#         s = perf_counter()
+#         ret = f(*args, **kwargs)
+#         e = perf_counter()
+
+#         print(e - s, 'sec')
+#         return ret
+    
+#     return wrap
+
+inf = float('inf')
+
+fmin = lambda x, y: x if x < y else y
+fmax = lambda x, y: x if x > y else y
+
+class Hash:
+    def __init__(self, arr, mul = 131, mod = 10 ** 9 + 7) -> None:
+        if arr[0].isalpha():
+            self.arr = [ord(c) for c in arr]
+        else:
+            self.arr = arr
+        self.mul = mul
+        self.mod = mod
+        self.n = len(arr)
+        self.mulpw = [1 for _ in range(self.n + 1)]
+        for i in range(1, self.n + 1):
+            self.mulpw[i] = self.mulpw[i - 1] * self.mul % self.mod
+
+        self.HASH = [0]
+        for c in self.arr:
+            self.HASH.append((self.HASH[-1] * self.mul + c) % self.mod)
+
+    #[l, r)
+    def GetHash(self, l, r):
+        return (self.HASH[r] - self.HASH[l] * self.mulpw[r - l]) % self.mod
+
+    #[l, r]
+    def CheckEqual(self, start, end, sz):
+        pattern = self.GetHash(0, sz)
+        for i in range(start, end - sz + 2, sz):
+            if self.GetHash(i, i + sz) != pattern:
+                return False
+        return True
+    
+    #[l, r)
+    def Check(self, ls, le, rs, re):
+        return self.GetHash(ls, le) == self.GetHash(rs, re)
+    
+    @staticmethod
+    def calc(arr):
+        if arr[0].isalpha():
+            arr = [ord(c) for c in arr]
+        else:
+            pass
+        mul = 131
+        mod = 10 ** 9 + 7
+        n = len(arr)
+        res = 0
+        
+        for c in arr:
+            res = (res * mul + c) % mod
+        
+        return res
+
+# @TIME
+def solve(testcase):
+    n, a, b, c = MI()
+    s = I()
+
+    H = Hash(s)
+
+    A = Hash.calc('nico')
+    B = Hash.calc('niconi')
+    C = Hash.calc('niconiconi')
+
+    res = [0 for _ in range(n + 1)]
+
+    for i in range(n):
+        res[i + 1] = fmax(res[i + 1], res[i])
+
+        if i >= 3 and H.GetHash(i - 3, i + 1) == A:
+            res[i + 1] = fmax(res[i + 1], res[i - 3] + a)
+        
+        if i >= 5 and H.GetHash(i - 5, i + 1) == B:
+            res[i + 1] = fmax(res[i + 1], res[i - 5] + b)
+        
+        if i >= 9 and H.GetHash(i - 9, i + 1) == C:
+            res[i + 1] = fmax(res[i + 1], res[i - 9] + c)
+        
+        # print(i, res[i + 1])
+    
+    print(max(res))
+
+for testcase in range(1):
+    solve(testcase)
