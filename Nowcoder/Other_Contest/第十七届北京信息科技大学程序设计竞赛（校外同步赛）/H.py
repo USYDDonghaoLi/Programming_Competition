897ad6b76352b5bf0d7aedb4bb568ddebf7d34cb
Ldh <ldh1315109@gmail.com>
2025-11-28 15:04:45 +1100

    Upload Codes for Nowcoder Contest

---
+++ Nowcoder/Other_Contest/第十七届北京信息科技大学程序设计竞赛（校外同步赛）/H.py
@@ -0,0 +1,318 @@
+'''
+Hala Madrid!
+https://github.com/USYDDonghaoLi/Programming_Competition
+'''
+
+import sys
+import os
+from io import BytesIO, IOBase
+BUFSIZE = 8192
+class FastIO(IOBase):
+    newlines = 0
+    def __init__(self, file):
+        self._fd = file.fileno()
+        self.buffer = BytesIO()
+        self.writable = "x" in file.mode or "r" not in file.mode
+        self.write = self.buffer.write if self.writable else None
+    def read(self):
+        while True:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            if not b:
+                break
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines = 0
+        return self.buffer.read()
+    def readline(self):
+        while self.newlines == 0:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            self.newlines = b.count(b"\n") + (not b)
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines -= 1
+        return self.buffer.readline()
+    def flush(self):
+        if self.writable:
+            os.write(self._fd, self.buffer.getvalue())
+            self.buffer.truncate(0), self.buffer.seek(0)
+class IOWrapper(IOBase):
+    def __init__(self, file):
+        self.buffer = FastIO(file)
+        self.flush = self.buffer.flush
+        self.writable = self.buffer.writable
+        self.write = lambda s: self.buffer.write(s.encode("ascii"))
+        self.read = lambda: self.buffer.read().decode("ascii")
+        self.readline = lambda: self.buffer.readline().decode("ascii")
+sys.stdin, sys.stdout = IOWrapper(sys.stdin), IOWrapper(sys.stdout)
+input = lambda: sys.stdin.readline().rstrip("\r\n")
+
+def I():
+    return input()
+def II():
+    return int(input())
+def MI():
+    return map(int, input().split())
+def LI():
+    return list(input().split())
+def LII():
+    return list(map(int, input().split()))
+def GMI():
+    return map(lambda x: int(x) - 1, input().split())
+
+#------------------------------FastIO---------------------------------
+
+from bisect import *
+from heapq import *
+from collections import *
+from functools import *
+from itertools import *
+from time import *
+from random import *
+from math import log, gcd, sqrt, ceil
+
+# from types import GeneratorType
+# def bootstrap(f, stack=[]):
+#     def wrappedfunc(*args, **kwargs):
+#         if stack:
+#             return f(*args, **kwargs)
+#         else:
+#             to = f(*args, **kwargs)
+#             while True:
+#                 if type(to) is GeneratorType:
+#                     stack.append(to)
+#                     to = next(to)
+#                 else:
+#                     stack.pop()
+#                     if not stack:
+#                         break
+#                     to = stack[-1].send(to)
+#             return to
+#     return wrappedfunc
+
+# seed(19981220)
+# RANDOM = getrandbits(64)
+ 
+# class Wrapper(int):
+#     def __init__(self, x):
+#         int.__init__(x)
+
+#     def __hash__(self):
+#         return super(Wrapper, self).__hash__() ^ RANDOM
+
+# def TIME(f):
+
+#     def wrap(*args, **kwargs):
+#         s = perf_counter()
+#         ret = f(*args, **kwargs)
+#         e = perf_counter()
+
+#         print(e - s, 'sec')
+#         return ret
+    
+#     return wrap
+
+inf = float('inf')
+
+fmin = lambda x, y: x if x < y else y
+fmax = lambda x, y: x if x > y else y
+
+class LazySegmentTree():
+    
+    __slots__ = ['n', 'log', 'size', 'd', 'lz', 'e', 'op', 'mapping', 'composition', 'identity']
+    
+    def _update(self, k):
+        self.d[k] = self.op(self.d[2 * k], self.d[2 * k + 1])
+    
+    def _all_apply(self, k, f):
+        self.d[k] = self.mapping(f, self.d[k])
+        if (k < self.size):
+            self.lz[k] = self.composition(f, self.lz[k])
+    
+    def _push(self, k):
+        self._all_apply(2 * k, self.lz[k])
+        self._all_apply(2 * k + 1, self.lz[k])
+        self.lz[k] = self.identity
+        
+    def __init__(self, V, OP, E, MAPPING, COMPOSITION, ID):
+        self.n = len(V)
+        self.log = (self.n - 1).bit_length()
+        self.size = 1 << self.log
+        self.d = [E for i in range(2 * self.size)]
+        self.lz = [ID for i in range(self.size)]
+        self.e = E
+        self.op = OP
+        self.mapping = MAPPING
+        self.composition = COMPOSITION
+        self.identity = ID
+        for i in range(self.n):
+            self.d[self.size + i] = V[i]
+            
+        for i in range(self.size - 1, 0, -1):
+            self._update(i)
+            
+    def set(self, p, x):
+        assert 0 <= p and p < self.n
+        p += self.size
+        for i in range(self.log, 0, -1):
+            self._push(p >> i)
+        self.d[p] = x
+        for i in range(1,self.log + 1):
+            self._update(p >> i)
+            
+    def get(self, p):
+        assert 0 <= p and p < self.n
+        p += self.size
+        for i in range(self.log, 0, -1):
+            self._push(p >> i)
+        return self.d[p]
+    
+    def prod(self, l, r):
+        assert 0 <= l and l <= r and r <= self.n
+        if l == r:
+            return self.e
+        l += self.size
+        r += self.size
+        for i in range(self.log, 0, -1):
+            if (((l >> i) << i) != l):
+                self._push(l >> i)
+            if (((r >> i) <<i) != r):
+                self._push(r >> i)
+        sml, smr = self.e, self.e
+        while (l < r):
+            if l & 1:
+                sml = self.op(sml, self.d[l])
+                l += 1
+            if r & 1:
+                r -= 1
+                smr = self.op(self.d[r], smr)
+            l >>= 1
+            r >>= 1
+        return self.op(sml, smr)
+    
+    def all_prod(self):
+        return self.d[1]
+    
+    def apply_point(self, p, f):
+        assert 0 <= p and p < self.n
+        p += self.size
+        for i in range(self.log, 0, -1):
+            self._push(p >> i)
+        self.d[p] = self.mapping(f, self.d[p])
+        for i in range(1, self.log + 1):
+            self._update(p >> i)
+            
+    def apply(self, l, r, f):
+        assert 0 <= l and l <= r and r <= self.n
+        if l == r:
+            return
+        l += self.size
+        r += self.size
+        for i in range(self.log, 0, -1):
+            if (((l >> i) << i) != l):
+                self._push(l >> i)
+            if (((r >> i) << i) != r):
+                self._push((r - 1) >> i)
+        l2, r2 = l, r
+        while (l < r):
+            if (l & 1):
+                self._all_apply(l, f)
+                l += 1
+            if (r & 1):
+                r -= 1
+                self._all_apply(r, f)
+            l >>= 1
+            r >>= 1
+        l, r = l2, r2
+        for i in range(1, self.log + 1):
+            if (((l >> i) << i) != l):
+                self._update(l >> i)
+            if (((r >> i) << i) != r):
+                self._update((r - 1) >> i)
+                
+    def max_right(self, l, g):
+        assert 0 <= l and l <= self.n
+        assert g(self.e)
+        if l == self.n:
+            return self.n
+        l += self.size
+        for i in range(self.log, 0, -1):
+            self._push(l >> i)
+        sm = self.e
+        while (1):
+            while (l % 2 == 0):
+                l >>= 1
+            if not(g(self.op(sm, self.d[l]))):
+                while(l < self.size):
+                    self._push(l)
+                    l = (2 * l)
+                    if (g(self.op(sm,self.d[l]))):
+                        sm = self.op(sm,self.d[l])
+                        l += 1
+                return l - self.size
+            sm = self.op(sm, self.d[l])
+            l += 1
+            if (l & -l) == l:break
+        return self.n
+    
+    def min_left(self,r,g):
+        assert 0 <= r and r <= self.n
+        assert g(self.e)
+        if r == 0:
+            return 0
+        r += self.size
+        for i in range(self.log, 0, -1):
+            self._push((r - 1) >> i)
+        sm = self.e
+        while (1):
+            r -= 1
+            while(r > 1 and (r % 2)):
+                r >>= 1
+            if not(g(self.op(self.d[r], sm))):
+                while (r < self.size):
+                    self._push(r)
+                    r = (2 * r + 1)
+                    if g(self.op(self.d[r], sm)):
+                        sm = self.op(self.d[r], sm)
+                        r -= 1
+                return r + 1 - self.size
+            sm = self.op(self.d[r], sm)
+            if (r & -r) == r:
+                break
+        return 0
+
+# @TIME
+def solve(testcase):
+    n = II()
+    A = LII()
+
+    B = []
+
+    for i, a in enumerate(A):
+        if i - a + 1 >= 0 and A[i - a + 1] <= a:
+            B.append((i - a + 1, i))
+        if i + a - 1 < n and A[i + a - 1] <= a:
+            B.append((i, i + a - 1))
+    
+    B.sort()
+
+    C = LazySegmentTree(
+        [inf for _ in range(n + 1)],
+        fmin,
+        inf,
+        lambda y, x: fmin(x, y),
+        lambda y, x: fmin(x, y),
+        inf
+    )
+
+    C.set(0, 0)
+
+    for l, r in B:
+        m = C.prod(l, n + 1)
+        C.apply(l + 1, r + 2, m + 1)
+    
+    res = C.get(n)
+
+    print(-1 if res == inf else res)
+
+for testcase in range(II()):
+    solve(testcase)
