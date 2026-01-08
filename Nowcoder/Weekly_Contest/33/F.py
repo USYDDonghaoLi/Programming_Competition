9255c16cc76e331f83803858afb8d06e0906080f
Ldh <ldh1315109@gmail.com>
2026-01-08 13:08:23 +1100

    Upload Code for Nowcoder Contest

--- Nowcoder/Weekly_Contest/33/F.py
+++ Nowcoder/Weekly_Contest/33/F.py
@@ -118,7 +118,173 @@ fmax = lambda x, y: x if x > y else y
 
 # @TIME
 def solve(testcase):
-    pass
+    n = II()
+    A = LII()
 
-for testcase in range(II()):
+    stack = []
+
+    res = 0
+
+    def f(l, r, nxt):
+        left, right = 0, 6 * 10 ** 9
+        LEN = r - l + 1
+        while left < right:
+            mid = left + right >> 1
+            sub = mid // LEN
+            
+            if r - sub >= nxt + mid:
+                left = mid + 1
+            else:
+                right = mid
+            # print("Check f", mid, sub, r - sub, nxt + mid, left, right, r - sub > nxt + mid)
+            
+        return left
+
+    def g(a):
+        nonlocal res
+        # print("Check Input of g", a)
+        l, r = inf, -inf
+
+        while a <= stack[-1][1]:
+            # print("Check a: ", a, stack)
+            l, r = stack.pop()
+            LEN = r - l + 1
+            if not stack:
+                need = f(l, r, a)
+                tot, rest = divmod(need, LEN)
+                if rest == 0:
+
+                    res += LEN * (LEN + 1) // 2 * tot
+
+                    a += need
+                    if a == r - tot + 1:
+                        stack.append([l - tot, r - tot + 1])
+                    else:
+                        stack.append([l - tot, r - tot])
+                        stack.append([a, a])
+                else:
+                    '''
+                    l, l + rest - 1, 减去tot + 1
+                    l + rest, r, 减去tot
+                    '''
+
+                    res += LEN * (LEN + 1) // 2 * tot + (LEN + LEN - rest + 1) * rest // 2
+
+                    a += need
+                    stack.append([l - tot - 1, l + rest - 1 - tot - 1])
+                    if a == r - tot + 1:
+                        stack.append([l + rest - tot, r + 1 - tot])
+                    else:
+                        stack.append([l + rest - tot, r - tot])
+                        stack.append([r + 1 - tot, r + 1 - tot])
+                break
+            
+            # print("Check stack in g 1:", stack)
+
+            r2 = stack[-1][1]
+            MAX = l - r2 - 1
+            # print("Check l, r, r2, MAX", l, r, r2, MAX, stack)
+            # assert MAX > 0
+
+            need = f(l, r, a)
+            tot, rest = divmod(need, LEN)
+            # print("Need", need, tot, rest)
+            
+            if rest == 0:
+                # print("Rest == 0", tot < MAX, tot == MAX)
+                if tot < MAX:
+
+                    res += LEN * (LEN + 1) // 2 * tot
+
+                    a += need
+                    if a == r - tot + 1:
+                        stack.append([l - tot, r - tot + 1])
+                    else:
+                        stack.append([l - tot, r - tot])
+                        stack.append([a, a])
+                    break
+                elif tot == MAX:
+
+                    res += LEN * (LEN + 1) // 2 * tot
+
+                    a += need
+                    l2, r2 = stack.pop()
+                    if a == r - tot + 1:
+                        stack.append([l2, r - tot + 1])
+                    else:
+                        stack.append([l2, r - tot])
+                        stack.append([a, a])
+                    # print("Case 2:", stack)
+                    break
+                else:
+
+                    res += LEN * (LEN + 1) // 2 * MAX
+
+                    tot_sub = MAX * LEN
+                    l2, r2 = stack.pop()
+                    stack.append([l2, r - MAX])
+                    a += tot_sub
+                    # print("Case 3:", stack, a)
+            else:
+                if tot + 1 < MAX:
+
+                    res += LEN * (LEN + 1) // 2 * tot + (LEN + LEN - rest + 1) * rest // 2
+
+                    a += need
+                    stack.append([l - tot - 1, l + rest - 1 - tot - 1])
+                    if a == r + 1 - tot:
+                        stack.append([l + rest - tot, r + 1 - tot])
+                    else:
+                        stack.append([l + rest - tot, r + rest - tot])
+                        stack.append([a, a])
+                elif tot + 1 == MAX:
+
+                    res += LEN * (LEN + 1) // 2 * tot + (LEN + LEN - rest + 1) * rest // 2
+
+                    a += need
+                    l2, r2 = stack.pop()
+                    stack.append([l2, l + rest - 1 - tot - 1])
+                    if a == r + 1 - tot:
+                        stack.append([l + rest - tot, r - tot + 1])
+                    else:
+                        stack.append([l + rest - tot, r + rest - tot])
+                        stack.append([a, a])
+                else:
+
+                    res += LEN * (LEN + 1) // 2 * MAX
+
+                    tot_sub = MAX * LEN
+                    l2, r2 = stack.pop()
+                    stack.append([l2, r - MAX])
+                    a += tot_sub
+
+
+    for a in A:
+        if not stack:
+            stack.append([a, a])
+            continue
+
+        # print("Check stack 1")
+
+        l, r = stack[-1]
+        if a > r + 1:
+            stack.append([a, a])
+        elif a == r + 1:
+            stack[-1][1] = r
+        else:
+            g(a)
+        
+    #     print("Check stack 2", stack)
+    
+    # print("Final Answer", stack)
+
+    final = []
+    for l, r in stack:
+        for i in range(l, r + 1):
+            final.append(i)
+    
+    print(res)
+    print(*final)
+
+for testcase in range(1):
     solve(testcase)
