566637a9e74264d10e9facb0b609b6f98a28fe57
USYDDonghaoLi <ldh1315109@gmail.com>
2026-02-27 08:40:38 +1100

    Upload Codes for Nowcoder Contest

---
+++ Nowcoder/Weekly_Contest/130/E.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int INF = 1000000;
+
+int main(){
+    
+    string s;
+    cin >> s;
+    
+    int n = s.size();
+    vector<int> A(n);
+    for (int i = 0; i < n; ++i){
+        A[i] = s[i] - 'a';
+    }
+    
+    int res = INF;
+    
+    for (int k = 0; k < 26; ++k){
+        
+        vector<vector<int>> B(n, vector<int>(26, INF));
+        for (int j = 0; j < 26; ++j){
+            if (A[0] == j){
+                B[0][j] = 0;
+            }
+            else{
+                B[0][j] = 1;
+            }
+        }
+        
+        for (int i = 1; i < n; ++i){
+            for (int j = 0; j < 26; ++j){
+                if (A[i] == j){
+                    B[i][j] = min(B[i][j], B[i - 1][(j - k + 26) % 26]);
+                    B[i][j] = min(B[i][j], B[i - 1][(j + k) % 26]);
+                }
+                else{
+                    B[i][j] = min(B[i][j], B[i - 1][(j - k + 26) % 26] + 1);
+                    B[i][j] = min(B[i][j], B[i - 1][(j + k) % 26] + 1);
+                }
+            }
+        }
+        
+        for (int j = 0; j < 26; ++j){
+            res = fmin(res, B[n - 1][j]);
+        }
+        
+    }
+    
+    cout << res << "\n";
+    
+    return 0;
+}
