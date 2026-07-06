9b16b4e3415958351359330db51d4a6559517c1f
USYDDonghaoLi <ldh1315109@gmail.com>
2026-07-07 02:29:19 +1000

    Upload Nowcoder Code for Weekly Contest

---
+++ Nowcoder/Weekly_Contest/151/D.py
@@ -0,0 +1,178 @@
+'''
+Hala Madrid!
+https://www.zhihu.com/people/li-dong-hao-78-74
+'''
+
+import sys
+import os
+from io import BytesIO, IOBase
+BUFSIZE = 8192
+class FastIO(IOBase):
+    newlines = 0
+    def __init__(self, file):
+        self._fd = file.fileno()
+        self.buffer = BytesIO()
+        self.writable = "x" in file.mode or "r" not in file.mode
+        self.write = self.buffer.write if self.writable else None
+    def read(self):
+        while True:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            if not b:
+                break
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines = 0
+        return self.buffer.read()
+    def readline(self):
+        while self.newlines == 0:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            self.newlines = b.count(b"\n") + (not b)
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines -= 1
+        return self.buffer.readline()
+    def flush(self):
+        if self.writable:
+            os.write(self._fd, self.buffer.getvalue())
+            self.buffer.truncate(0), self.buffer.seek(0)
+class IOWrapper(IOBase):
+    def __init__(self, file):
+        self.buffer = FastIO(file)
+        self.flush = self.buffer.flush
+        self.writable = self.buffer.writable
+        self.write = lambda s: self.buffer.write(s.encode("ascii"))
+        self.read = lambda: self.buffer.read().decode("ascii")
+        self.readline = lambda: self.buffer.readline().decode("ascii")
+sys.stdin, sys.stdout = IOWrapper(sys.stdin), IOWrapper(sys.stdout)
+input = lambda: sys.stdin.readline().rstrip("\r\n")
+
+def I():
+    return input()
+def II():
+    return int(input())
+def MI():
+    return map(int, input().split())
+def LI():
+    return list(input().split())
+def LII():
+    return list(map(int, input().split()))
+def GMI():
+    return map(lambda x: int(x) - 1, input().split())
+def LGMI():
+    return list(map(lambda x: int(x) - 1, input().split()))
+
+#------------------------------FastIO---------------------------------
+
+from bisect import *
+from heapq import *
+from collections import *
+from functools import *
+from itertools import *
+from time import *
+from random import *
+from math import log, gcd, sqrt, ceil
+
+# from types import GeneratorType
+# def bootstrap(f, stack=[]):
+#     def wrappedfunc(*args, **kwargs):
+#         if stack:
+#             return f(*args, **kwargs)
+#         else:
+#             to = f(*args, **kwargs)
+#             while True:
+#                 if type(to) is GeneratorType:
+#                     stack.append(to)
+#                     to = next(to)
+#                 else:
+#                     stack.pop()
+#                     if not stack:
+#                         break
+#                     to = stack[-1].send(to)
+#             return to
+#     return wrappedfunc
+
+# seed(19981220)
+# RANDOM = getrandbits(64)
+ 
+# class Wrapper(int):
+#     def __init__(self, x):
+#         int.__init__(x)
+
+#     def __hash__(self):
+#         return super(Wrapper, self).__hash__() ^ RANDOM
+
+# def TIME(f):
+
+#     def wrap(*args, **kwargs):
+#         s = perf_counter()
+#         ret = f(*args, **kwargs)
+#         e = perf_counter()
+
+#         print(e - s, 'sec')
+#         return ret
+    
+#     return wrap
+
+inf = float('inf')
+
+fmin = lambda x, y: x if x < y else y
+fmax = lambda x, y: x if x > y else y
+
+d = ((0, 1), (1, 0), (-1, 0), (0, -1))
+
+# @TIME
+def solve(testcase):
+    n, m = MI()
+    a, b = MI()
+    A = [I() for _ in range(n)]
+    sx, sy = -1, -1
+    tx, ty = -1, -1
+
+    B = [[0 for _ in range(m + 1)] for _ in range(n + 1)]
+
+    def f(x1, y1, x2, y2):
+        return B[x2 + 1][y2 + 1] - B[x1][y2 + 1] - B[x2 + 1][y1] + B[x1][y1]
+
+    for i in range(n):
+        for j in range(m):
+            if A[i][j] == 'S':
+                sx, sy = i, j
+            elif A[i][j] == 'E':
+                tx, ty = i, j
+            elif A[i][j] == '#':
+                B[i + 1][j + 1] = 1
+    
+    for i in range(1, n + 1):
+        for j in range(1, m + 1):
+            B[i][j] += B[i - 1][j] + B[i][j - 1] - B[i - 1][j - 1]
+    
+    # print("B", B)
+    
+    dist = [[inf for _ in range(m)] for _ in range(n)]
+    dist[sx][sy] = 0
+    
+    q = deque()
+    q.append((sx, sy))
+
+    while q:
+        k = len(q)
+        for _ in range(k):
+            x, y = q.popleft()
+            if x == tx and y == ty:
+                print("Yes")
+                return
+            
+            for dx, dy in d:
+                nx, ny = x + dx, y + dy
+                nnx, nny = nx + a - 1, ny + b - 1
+
+                if 0 <= nx < n and 0 <= ny < m and 0 <= nnx < n and 0 <= nny < m:
+
+                    if not f(nx, ny, nnx, nny) and dist[nx][ny] > dist[x][y] + 1:
+                        dist[nx][ny] = dist[x][y] + 1
+                        q.append((nx, ny))
+    
+    print("No")
+
+for testcase in range(II()):
+    solve(testcase)
