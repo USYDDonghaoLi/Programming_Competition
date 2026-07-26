31002b81bec7d7686c0477f545995323aa27ac74
USYDDonghaoLi <ldh1315109@gmail.com>
2026-07-26 19:35:48 +1000

    Upload Code for Nowcoder Weekly Contest 65

---
+++ Nowcoder/Weekly_Contest/65/G.py
@@ -0,0 +1,188 @@
+"""
+Fast IO（快速输入输出）
+
+用于竞争编程的高性能 I/O 模板。
+
+特点：
+- 使用缓冲减少系统调用
+- 快速读写整数和字符串
+- 适合输入输出量大的题目
+
+使用：
+- input() / readline() 读取
+- 自定义输出函数
+"""
+
+'''
+Hala Madrid!
+https://github.com/USYDDonghaoLi/Programming_Competition
+'''
+
+import sys
+import os
+from io import BytesIO, IOBase
+BUFSIZE = 8192
+class FastIO(IOBase):
+    newlines = 0
+    def __init__(self, file):
+        self._fd = file.fileno()
+        self.buffer = BytesIO()
+        self.writable = "x" in file.mode or "r" not in file.mode
+        self.write = self.buffer.write if self.writable else None
+    def read(self):
+        while True:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            if not b:
+                break
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines = 0
+        return self.buffer.read()
+    def readline(self):
+        while self.newlines == 0:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            self.newlines = b.count(b"\n") + (not b)
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines -= 1
+        return self.buffer.readline()
+    def flush(self):
+        if self.writable:
+            os.write(self._fd, self.buffer.getvalue())
+            self.buffer.truncate(0), self.buffer.seek(0)
+class IOWrapper(IOBase):
+    def __init__(self, file):
+        self.buffer = FastIO(file)
+        self.flush = self.buffer.flush
+        self.writable = self.buffer.writable
+        self.write = lambda s: self.buffer.write(s.encode("ascii"))
+        self.read = lambda: self.buffer.read().decode("ascii")
+        self.readline = lambda: self.buffer.readline().decode("ascii")
+sys.stdin, sys.stdout = IOWrapper(sys.stdin), IOWrapper(sys.stdout)
+input = lambda: sys.stdin.readline().rstrip("\r\n")
+
+def I():
+    return input()
+def II():
+    return int(input())
+def MI():
+    return map(int, input().split())
+def LI():
+    return list(input().split())
+def LII():
+    return list(map(int, input().split()))
+def GMI():
+    return map(lambda x: int(x) - 1, input().split())
+def LGMI():
+    return list(map(lambda x: int(x) - 1, input().split()))
+
+#------------------------------FastIO---------------------------------
+
+from bisect import *
+from heapq import *
+from collections import *
+from functools import *
+from itertools import *
+from time import *
+from random import *
+from math import log, gcd, sqrt, ceil
+
+# from types import GeneratorType
+# def bootstrap(f, stack=[]):
+#     def wrappedfunc(*args, **kwargs):
+#         if stack:
+#             return f(*args, **kwargs)
+#         else:
+#             to = f(*args, **kwargs)
+#             while True:
+#                 if type(to) is GeneratorType:
+#                     stack.append(to)
+#                     to = next(to)
+#                 else:
+#                     stack.pop()
+#                     if not stack:
+#                         break
+#                     to = stack[-1].send(to)
+#             return to
+#     return wrappedfunc
+
+# seed(19981220)
+# RANDOM = getrandbits(64)
+ 
+# class Wrapper(int):
+#     def __init__(self, x):
+#         int.__init__(x)
+
+#     def __hash__(self):
+#         return super(Wrapper, self).__hash__() ^ RANDOM
+
+# def TIME(f):
+
+#     def wrap(*args, **kwargs):
+#         s = perf_counter()
+#         ret = f(*args, **kwargs)
+#         e = perf_counter()
+
+#         print(e - s, 'sec')
+#         return ret
+    
+#     return wrap
+
+inf = float('inf')
+
+fmin = lambda x, y: x if x < y else y
+fmax = lambda x, y: x if x > y else y
+
+# @TIME
+def solve(testcase):
+    n, m = MI()
+
+    l, r = 0, 10 ** 18 + 10
+
+    while l < r:
+        mid = l + r >> 1
+        cnt = n * mid + n * (n - 1) // 2
+        if cnt < m:
+            l = mid + 1
+        else:
+            r = mid
+    
+    # print("l", l)
+
+    A = [1 for _ in range(n - 1)]
+
+    for k in range(l, l + 2):
+        if k < 0:
+            continue
+        lower = n * k - n * (n - 1) // 2
+        upper = n * k + n * (n - 1) // 2
+        # print(k, lower, upper)
+        if lower > m:
+            break
+
+        if lower <= m <= upper and lower & 1 == m & 1:
+            # print("ok", k, lower, upper)
+            diff = (upper - m) // 2
+            cur = k
+
+            for i in range(n - 1):
+                change = n - 1 - i
+                if diff >= change and cur > 0:
+                    A[i] = -1
+                    diff -= change
+                cur += A[i]
+            
+            if diff == 0:
+                res = [k]
+                for a in A:
+                    res.append(res[-1] + a)
+                
+                print(*res)
+                return
+            
+    
+    print(-1)
+
+
+for testcase in range(1):
+    solve(testcase)
